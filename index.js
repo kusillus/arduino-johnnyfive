c8ce458f6bed108cbc645e66e8938d16cf0d519b
Alexander Guevara <wguevarab7@gmail.com>
2018-10-17 23:13:49 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const five = require("johnny-five");
+const board = new five.Board({
+    "port": "COM3"
+});
+
+board.on("ready", function() {
+    const leds = new five.Leds([11,10,9]);
+    const ledCount = leds.length;
+    const timing = 400;
+    var fadeIndex = 0;
+    
+
+    function fadeNext() {
+        var candidateIndex = fadeIndex;
+        leds[fadeIndex].fadeIn(timing);
+        while (candidateIndex === fadeIndex) {            
+            candidateIndex = Math.round(Math.random() * (ledCount - 1));
+            console.log('Number of led: '+leds[candidateIndex].pin);
+        }
+        fadeIndex = candidateIndex;
+        leds[fadeIndex].fadeOut(timing, fadeNext);
+    }
+    leds.on();
+    leds[fadeIndex].fadeOut(timing, fadeNext);
+});
